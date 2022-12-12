b8a2c6a8faec2e54127e3675462ff59d6a5c8801
Lysenko <lysenkoden@gmail.com>
2022-12-12 22:30:14 +0200

    add components search bar and bodyParts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState } from "react";
+import { Box } from "@mui/material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+    </Box>
+  );
 };
 
 export default Home;
